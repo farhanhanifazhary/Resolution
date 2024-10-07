df3079f1a2a496cc04b09c725c6f9c2360c9333a
farhanhanifazhary <farhanhanifazhary@gmail.com>
2024-10-07 07:43:28 +0700

    Delete Kuliah directory

--- Kuliah/Pertemuan 1/Untitled1.c
+++
@@ -1,7 +0,0 @@
-//prinf => Perintah menampilkan output
-//scanf => Perintahn menambahkan input
-
-//TIpe data
-
-//Angka : int (%i, %d) => bilangan bulat, long int(%li) => bilangan bulat dengan karakter lebih panjang, float (%f) => bil. pecahan, double => 2x float
-//Tulisan/huruf : char => huruf, string/varchar => karakternya berbatas (ratusan ribu), text => karakternya banyak
