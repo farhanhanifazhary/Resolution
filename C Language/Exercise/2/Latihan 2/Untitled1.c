d48e8b06925dbb82a92cda6e3f562142ac843e0c
farhanhanifazhary <farhanhanifazhary@gmail.com>
2024-11-17 08:45:53 +0700

    Delete C Language/Exercise directory

--- C Language/Exercise/2/Latihan 2/Untitled1.c
+++
@@ -1,2 +0,0 @@
-//fungsi => dimulai dari tipe data, diakhiri dengan nilai kembalian
-//prosedur => merapikan codingan => dimulai dengan void
--- C Language/Exercise/New folder/Extra 2.c
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-
-void main() {
-    float pi;
-
-    pi = 3.14;
-    printf("pi = %f", pi);
-}
